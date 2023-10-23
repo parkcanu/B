06174b855e3da870df5cb2d9fc47e90d1eec9263
Park chanwoo <chanu002@daum.net>
2023-10-23 17:43:41 +0900

    v1.7

---
+++ week9/ma.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
+
+func main() {
+
+	a := 1.8
+	b := 2.7
+
+	c := &a // var c *int = &a
+	// fmt.Printf("%d %d %x\n", a, b, c)
+	fmt.Printf("%d %d %d\n", a, b, *c)
+}
