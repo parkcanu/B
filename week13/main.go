827c83f35353708d0e5e662fcfdac740f90a7443
Park chanwoo <chanu002@daum.net>
2023-11-20 16:52:46 +0900

    v 13.1

---
+++ week13/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a []string
+	var b []bool
+	// a = make([]string, 4, 5)
+	b = append(b, true)
+	fmt.Printf("%#v %#v\n", a, b)
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
+}
